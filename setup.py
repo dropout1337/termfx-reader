8a69749dde15eb22502ed651b3d474ff3aa3da53
dropout <dropout@fbi.ac>
2022-04-06 19:44:44 +1200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="termfx-reader",
+    author="dropout / null",
+    description="Termfx reader for python",
+    url="https://github.com/dropout1337/termfx-reader",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    include_package_data=True,
+    version="1.0.0"
+)
